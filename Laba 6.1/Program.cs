68f81c1340826631a9f5b3e97c79338c1dadec45
Kirilo Chori <kirilosakanaliev@gmail.com>
2024-11-14 16:23:38 +0200

    Добавьте файлы проекта.

---
+++ Laba 6.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Laba_6._1
+{
+    internal class Program
+    {
+        static double Func(double x, double y)
+        {
+            return (Math.Sinh(x) * Math.Tan(x + 1)) - Math.Pow(1 / Math.Tan(2 + Math.Sinh(x - 1)), 2);
+        }
+
+        static void Main()
+        {
+            double x = Func(-2.5, 0.5) + Func(0, 2) + Func(2, 5);
+            Console.WriteLine("Значення функції: " + x);
+        }
+    }
+
+
+}   
+
+
+
+
